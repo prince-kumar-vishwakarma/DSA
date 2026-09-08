a0e211cd4a291e5ccfbe18510dcffc3d9d13abc9
Prince Kumar Vishwakarma <147078284+prince-kumar-vishwakarma@users.noreply.github.com>
2026-09-08 19:14:39 +0530

    3870 Daily question

---
+++ 3870_E_LC.py
@@ -0,0 +1,8 @@
+# 3870. Count Commas in Range
+# https://leetcode.com/problems/count-commas-in-range/
+
+class Solution:
+    def countCommas(self, n: int) -> int:
+        if n<1000: return 0
+        return n-1000+1
+        
