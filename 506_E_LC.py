07fd6bb280d1758de32c9e1d4cd61cd9c13436cb
Prince Kumar Vishwakarma <147078284+prince-kumar-vishwakarma@users.noreply.github.com>
2026-09-22 19:50:20 +0530

    Another good solution of 506

--- 506_E_LC.py
+++ 506_E_LC.py
@@ -21,6 +21,26 @@ class Solution:
             score[i] = hm[n]
         return score
 
+        #===================== Another Good Solution ======================#
+    def findRelativeRanks_sol2(self, score: list[int]) -> list[str]:
+        temp = [(-x,i) for i,x in enumerate(score)]
+        heapq.heapify(temp)
+        rank = 1
+        while temp:
+            _, i = heapq.heappop(temp)
+            if rank == 1:
+                score[i] = "Gold Medal"
+            elif rank == 2:
+                score[i] = "Silver Medal"
+            elif rank == 3:
+                score[i] = "Bronze Medal"
+            else:
+                score[i] = f"{rank}"
+            rank += 1
+        return score
+
+
+
 
 
 
